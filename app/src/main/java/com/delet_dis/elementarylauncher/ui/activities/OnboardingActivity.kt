a99fbb94520f5140a9ba1a839f32d08dc372355a
delet-dis <igor@efimov.ga>
2021-05-31 00:46:33 +0700

    updated horizontal layouts for launcher activity fragments

--- app/src/main/java/com/delet_dis/elementarylauncher/ui/activities/OnboardingActivity.kt
+++ app/src/main/java/com/delet_dis/elementarylauncher/ui/activities/OnboardingActivity.kt
@@ -7,7 +7,6 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProviderInfo
 import android.content.Intent
 import android.content.pm.ApplicationInfo
-import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
