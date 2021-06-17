5d039e71afa5df9e07301bf9ac1bc4dc5e9dc3d7
delet-dis <igor@efimov.ga>
2021-06-17 20:09:18 +0700

    ⬆ moved application upper in project structure

---
+++ app/src/main/java/com/delet_dis/elementarylauncher/ElementaryLauncherApplication.kt
@@ -0,0 +1,7 @@
+package com.delet_dis.elementarylauncher
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ElementaryLauncherApplication : Application()
--- app/src/main/java/com/delet_dis/elementarylauncher/data/common/di/ElementaryLauncherApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.delet_dis.elementarylauncher.data.common.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ElementaryLauncherApplication:Application()
