2840dfab85fe7f71ffb2cd09e4cbb534d4b70e6c
delet-dis <igor@efimov.ga>
2021-07-18 00:54:45 +0700

    ➕ trying to add seekbar test

---
+++ app/src/androidTest/java/com/delet_dis/elementarylauncher/extensions/SliderExtensions.kt
@@ -0,0 +1,41 @@
+package com.delet_dis.elementarylauncher.extensions
+
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatSeekBar
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers
+import com.google.android.material.slider.Slider
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
+fun withValue(expectedValue: Int): Matcher<View?> {
+    return object : BoundedMatcher<View?, AppCompatSeekBar>(AppCompatSeekBar::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("expected: $expectedValue")
+        }
+
+        override fun matchesSafely(seekBar: AppCompatSeekBar?): Boolean {
+            return seekBar?.progress == expectedValue
+        }
+    }
+}
+
+fun setValue(value: Int): ViewAction {
+    return object : ViewAction {
+        override fun getDescription(): String {
+            return "Set Slider value to $value"
+        }
+
+        override fun getConstraints(): Matcher<View> {
+            return ViewMatchers.isAssignableFrom(Slider::class.java)
+        }
+
+        override fun perform(uiController: UiController?, view: View) {
+            val seekBar = view as AppCompatSeekBar
+            seekBar.progress = value
+        }
+    }
+}
---
+++ app/src/androidTest/java/com/delet_dis/elementarylauncher/presentation/activities/onboardingActivity/valuesTests/ChangeScaleInOnboarding.kt
@@ -0,0 +1,76 @@
+package com.delet_dis.elementarylauncher.presentation.activities.onboardingActivity.valuesTests
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.delet_dis.elementarylauncher.R
+import com.delet_dis.elementarylauncher.extensions.childAtPosition
+import com.delet_dis.elementarylauncher.extensions.setValue
+import com.delet_dis.elementarylauncher.extensions.withValue
+import com.delet_dis.elementarylauncher.presentation.activities.onboardingActivity.OnboardingActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
+class ChangeScaleInOnboarding {
+
+    @ExperimentalCoroutinesApi
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(OnboardingActivity::class.java)
+
+    @Test
+    fun sliderInInterfaceScalePickFragmentValueChangerTest() {
+        val getStartedButton = onView(
+            allOf(
+                withId(R.id.getStartedButton), withText(R.string.getStartedButtonText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigationOnboardingControllerContainerView),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        getStartedButton.perform(click())
+
+        val nextButtonInScreenLayoutPickFragment = onView(
+            allOf(
+                withId(R.id.nextButton), withText(R.string.nextButtonText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.navigationOnboardingControllerContainerView),
+                        0
+                    ),
+                    7
+                ),
+                isDisplayed()
+            )
+        )
+        nextButtonInScreenLayoutPickFragment.perform(click())
+
+        val interfaceScaleSeekBar = onView(
+            withId(R.id.interfaceScaleSeekBar)
+        )
+
+        interfaceScaleSeekBar.perform(setValue(50))
+        interfaceScaleSeekBar.check(matches(withValue(50)))
+
+//        interfaceScaleSeekBar.check(matches(withValue(3)))
+
+
+
+        interfaceScaleSeekBar.perform(setValue(50))
+        interfaceScaleSeekBar.check(matches(withValue(50)))
+    }
+}
