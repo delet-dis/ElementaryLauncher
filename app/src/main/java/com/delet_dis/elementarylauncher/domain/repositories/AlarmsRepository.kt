2eb4ce7d561dd05b09d41ac9e7bd76b0eef115ad
delet-dis <efimov.036@gmail.com>
2021-08-23 14:43:30 +0700

    ➡️ moved launcher activity and its components to di

--- app/src/main/java/com/delet_dis/elementarylauncher/domain/repositories/AlarmsRepository.kt
+++ app/src/main/java/com/delet_dis/elementarylauncher/domain/repositories/AlarmsRepository.kt
@@ -2,8 +2,15 @@ package com.delet_dis.elementarylauncher.domain.repositories
 
 import android.app.AlarmManager
 import android.content.Context
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 
-class AlarmsRepository(val context: Context) {
+@Module
+@InstallIn(SingletonComponent::class)
+class AlarmsRepository @Inject constructor(@ApplicationContext private val context: Context) {
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
     var nextAlarm: Long? =
